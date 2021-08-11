132905c4db699d3a982ff0cdd8ddde24eac0c850
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-11 17:29:46 -0300

    feat: create entity

---
+++ chat_api/code/src/entities/User.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity("users")
+class User {
+
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
+
+export { User }
