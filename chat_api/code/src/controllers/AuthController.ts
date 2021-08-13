39f01acc82e82f23506474ffe1313b7393253eaa
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-13 20:06:36 -0300

    feat: implement auth controllers and services

---
+++ chat_api/code/src/controllers/AuthController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from 'express'
+import { AuthService } from '../services/AuthService'
+
+class AuthController {
+
+  async register(request: Request, response: Response): Promise<Response> {
+    try {
+      const authService = new AuthService()
+
+      const { name, email, password } = request.body
+
+      const result = await authService.register({
+        email,
+        name,
+        password
+      })
+
+      return response.status(201).send({
+        success: true,
+        data: result
+      })
+
+    } catch (error) {
+
+      return response.status(400).send({
+        error: true,
+        message: error.message
+      })
+    }
+  }
+
+  async authenticate(request: Request, response: Response): Promise<Response> {
+
+    try {
+
+      const authService = new AuthService()
+      const { email, password } = request.body
+
+      const token = await authService.authenticate(email, password)
+
+      return response.status(200).send({
+        success: true,
+        data: {
+          token
+        }
+      })
+
+    } catch (error) {
+      return response.status(401).send({
+        error: true,
+        message: error.message
+      })
+    }
+
+  }
+}
+
+
+export { AuthController }
---
+++ chat_api/code/src/services/AuthService.ts
@@ -0,0 +1,46 @@
+import { UserProps } from './../entities/User';
+import { UserService } from './UserService';
+import { JwtAdapter } from '../adapters/JwtAdapter';
+import { EncrypterAdapter } from '../adapters/EncrypterAdapter'
+
+class AuthService {
+
+  async authenticate(email: string, password: string) {
+    const userService = new UserService()
+    const user = await userService.findUserEmail(email)
+    const encrypter = new EncrypterAdapter()
+    const jwt = new JwtAdapter()
+
+    if (!user) {
+      throw new Error('User not found!')
+    }
+
+    if (!encrypter.verifyPassword(password, user.password)) {
+      throw new Error('Incorrect password')
+    }
+
+    const token = await jwt.generateToken({ ...user }, "8h")
+
+    return token
+
+  }
+
+  async register(data: UserProps) {
+
+    const userService = new UserService()
+
+    const { email } = data
+    const user = await userService.findUserEmail(email)
+
+    if (user) {
+      throw new Error('User already exists!')
+    }
+
+    const parsedUser = await userService.create(data)
+
+    return parsedUser
+
+  }
+}
+
+export { AuthService }
