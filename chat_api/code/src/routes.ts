d3dc963a25672e6c99d0fb6e20a42b7da923f187
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-13 20:07:32 -0300

    refactor: fix routes

--- chat_api/code/src/routes.ts
+++ chat_api/code/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { AuthController } from './controllers/AuthController'
 import { UserController } from './controllers/UserController'
 const routes = Router()
 
 const userController = new UserController()
+const authController = new AuthController()
 
-routes.post('/users', userController.register)
+routes.post('/register', authController.register)
+routes.post('/auth', authController.authenticate)
 
 export { routes }
