2e9b047cc9fcb872913974dd62a4f96b7095712d
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-14 17:06:04 -0300

    refactor: fix routes

--- chat_api/code/src/routes.ts
+++ chat_api/code/src/routes.ts
@@ -3,17 +3,30 @@ import { ValidateToken } from './middleware/ValidateToken'
 import { AuthController } from './controllers/AuthController'
 import { UserController } from './controllers/UserController'
 import { MessageController } from './controllers/MessageController'
+import { RoomController } from './controllers/RoomController'
+
 const validateToken = new ValidateToken()
 const routes = Router()
+
 routes.use(validateToken.validate)
 
 const userController = new UserController()
 const authController = new AuthController()
 const messageController = new MessageController()
+const roomController = new RoomController()
 
+//Auth
 routes.post('/register', authController.register)
 routes.post('/auth', authController.authenticate)
+
+//Users
 routes.get('/users', userController.getAll)
 
+//Rooms
+routes.post('/rooms', roomController.create)
+routes.get('/rooms', roomController.getRooms)
+
+//Messages
 routes.post('/messages', messageController.create)
+
 export { routes }
