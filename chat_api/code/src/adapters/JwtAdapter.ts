a422132fdd768b7e1e792925f08a659676b0ddf0
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-13 21:06:13 -0300

    feat: implements verify token function

--- chat_api/code/src/adapters/JwtAdapter.ts
+++ chat_api/code/src/adapters/JwtAdapter.ts
@@ -2,9 +2,13 @@ import jwt from 'jsonwebtoken'
 const secret = process.env.JWT_SECRET
 class JwtAdapter {
 
-  async generateToken(payload: any, ttl: string | number) {
+  async generateToken(payload: any, ttl: string | number): Promise<string> {
 
     return await jwt.sign(payload, secret, { expiresIn: ttl })
+  }
+
+  verify(token: string) {
+    return jwt.verify(token.replace('Bearer ', ''), secret, { ignoreExpiration: false })
   }
 }
 
