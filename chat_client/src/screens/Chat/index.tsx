5e45e1f0af2701d22dee91b146a419b9501c9e54
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-14 22:30:48 -0300

    feat: create chat component

--- chat_client/src/screens/Chat/index.tsx
+++ chat_client/src/screens/Chat/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Avatar from '../../components/Avatar'
+import './style.css'
+import { AuthContext } from '../../context/AuthContext'
+import { Add } from '@material-ui/icons'
 
-// import { Container } from './styles';
+import avatar from '../../assets/avatar.jpg'
 
 const Chat: React.FC = () => {
-  return <div />
+  const { user } = useContext(AuthContext)
+
+  return (
+    <div className='container'>
+      <div className="sidebar">
+        <div className="user-info">
+          <Avatar avatar={avatar}/>
+          <span className='info'>
+            <span className='title'>{`Olá ${user}`}</span>
+            <span className='subtitle'>Seja bem vindo!</span>
+          </span>
+          <button onClick={() => console.log('ajsakljsalk')} className='btn'>
+          <Add style={{ color: '#fff' }} />
+          </button>
+        </div>
+      </div>
+      <div className="message-box"></div>
+    </div>
+  )
 }
 
 export default Chat
