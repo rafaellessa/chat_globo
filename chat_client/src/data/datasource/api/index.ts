c68896c1a219e3979ba9f8d7b65959481a06349b
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-15 22:12:49 -0300

    feat: implement axios instance

---
+++ chat_client/src/data/datasource/api/index.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosInstance } from 'axios'
+export default class Api {
+  private static apiInstance: AxiosInstance;
+  public static getApiInstance (baseUrl: string): AxiosInstance {
+    const api = axios.create({
+      baseURL: baseUrl,
+      timeout: 1200,
+      withCredentials: true,
+      headers: {
+        accept: 'application/json, text/plain, */*',
+        'content-type': 'application/json',
+        'cache-control': 'no-store'
+      }
+    })
+    this.apiInstance = api
+    return this.apiInstance
+  }
+}
