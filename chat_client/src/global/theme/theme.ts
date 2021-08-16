5a21555d52fa0657d1cd6721bb3e68bbc3449611
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-15 21:47:55 -0300

    feat: create theme styled

---
+++ chat_client/src/global/theme/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#128C7E',
+    secondary: '#075e54',
+    secondary100: '#6FCF97',
+    secondary200: '#25d366',
+    secondary300: '#34b7f1',
+    secondary400: '#DDD3D3',
+    secondary500: '#ECE5DD',
+    secondary600: '#767474',
+    secondary700: '#5a5959',
+    white: '#fff'
+
+  }
+}
