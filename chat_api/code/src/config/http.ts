f8da1ffc8cc0c5091354405bce6636853abaf839
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-17 20:42:56 -0300

    refactor: config http and socket

--- chat_api/code/src/config/http.ts
+++ chat_api/code/src/config/http.ts
@@ -1,10 +1,10 @@
+/* eslint-disable import/first */
 
+import cors from 'cors'
 import express from 'express'
 import { createServer } from 'http'
-import { routes } from '../routes'
 import '../database'
-import { Server, Socket } from 'socket.io'
-import cors from 'cors'
+import { routes } from '../routes'
 const app = express()
 const corsOptions = {
   origin: 'http://localhost:3000'
@@ -15,19 +15,5 @@ app.use(cors(corsOptions))
 app.use(routes)
 
 const http = createServer(app)
-const io = new Server(http)
-
-io.on('connection', (socket: Socket) => {
-  console.log('Socket conectado', socket.id)
-
-  socket.on('chat.message', (data) => {
-    console.log('mensagem recebida: ', data)
-  })
-
-  socket.emit('chat.message', {
-    id: 2,
-    message: 'recebido'
-  })
-})
-
-export { http, io }
+import '../websocket/socket'
+export { http }
