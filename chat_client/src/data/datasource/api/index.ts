176dd7cf4dff55867830bc8a4f9fdbea05d061a5
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-17 21:22:32 -0300

    feat: save token localstorage

--- chat_client/src/data/datasource/api/index.ts
+++ chat_client/src/data/datasource/api/index.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosInstance } from 'axios'
+
+const token = localStorage.getItem('token')
 export default class Api {
   private static apiInstance: AxiosInstance;
   public static getApiInstance (baseUrl: string): AxiosInstance {
     const api = axios.create({
       baseURL: baseUrl,
-      timeout: 1200
+      timeout: 1200,
+      headers: { Authorization: 'Bearer ' + token }
     })
     this.apiInstance = api
     return this.apiInstance
