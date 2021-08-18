e8c1508a1679c3e2bfb03bd1d22376518beb3a16
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-18 16:59:13 -0300

    fix: fix

--- chat_api/code/src/services/UserService.ts
+++ chat_api/code/src/services/UserService.ts
@@ -1,36 +1,33 @@
-import { getCustomRepository, Repository } from 'typeorm';
-import { User, UserProps } from "../entities/User";
-import { UsersRepository } from '../repositories/UsersRepository';
+import { getCustomRepository, Repository } from 'typeorm'
+import { User, UserProps } from '../entities/User'
+import { UsersRepository } from '../repositories/UsersRepository'
 
 class UserService {
-
   private userRepository: Repository<User>
-  constructor() {
+  constructor () {
     this.userRepository = getCustomRepository(UsersRepository)
   }
 
-  async findUserEmail(email: string) {
+  async findUserEmail (email: string) {
     const repo = getCustomRepository(UsersRepository)
     const user = await repo.findByEmail(email)
     return user
   }
 
-  async create(data: UserProps) {
-
+  async create (data: UserProps) {
     const parsedUser = this.userRepository.create({ ...data })
     await this.userRepository.save(parsedUser)
 
     parsedUser.password = undefined
 
-    return parsedUser;
+    return parsedUser
+  }
+
+  async getUser (id: string) {
 
   }
 
-  async getUser(id: string) {
-
-  }
-
-  async getUsers() {
+  async getUsers () {
     const repo = getCustomRepository(UsersRepository)
     const users = await repo.getAllUsers()
     return users
